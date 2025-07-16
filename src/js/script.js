027366827a1fdc7b0732f894efcaef2e7c976003
Srujan P <srujan.r.patil@gmail.com>
2025-07-16 17:38:01 -0400

    fixed js error

--- src/js/script.js
+++ src/js/script.js
@@ -629,25 +629,26 @@ function createPortfolioDropdown(items) {
 // JavaScript for additional email validation
 const form = document.querySelector('#contactForm');
 const email = document.querySelector('#email');
+if (form && email) {
+    form.addEventListener('submit', function (event) {
+        // Simple email regex for validation
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-form.addEventListener('submit', function (event) {
-    // Simple email regex for validation
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email.value)) {
+            // Prevent form submission if the email is invalid
+            event.preventDefault();
 
-    if (!emailRegex.test(email.value)) {
-        // Prevent form submission if the email is invalid
-        event.preventDefault();
+            // Set a custom validity message
+            email.setCustomValidity('Please enter a valid email address.');
+        } else {
+            // Reset the custom validity if the input is valid
+            email.setCustomValidity('');
+        }
+    });
 
-        // Set a custom validity message
-        email.setCustomValidity('Please enter a valid email address.');
-    } else {
-        // Reset the custom validity if the input is valid
-        email.setCustomValidity('');
-    }
-});
-
-// Reset custom validity message on every input change
-email.addEventListener('input', function () {
-    email.setCustomValidity(''); // Reset the custom error on input change
-});
+    // Reset custom validity message on every input change
+    email.addEventListener('input', function () {
+        email.setCustomValidity(''); // Reset the custom error on input change
+    });
+}
 /********* End JS for the contact form *********/
